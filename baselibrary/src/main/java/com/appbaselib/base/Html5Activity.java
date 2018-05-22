6bdcd3d3466dc0437776b1123b33fe4ce7109129
tangming <1079240451@qq.com>
2018-05-22 08:15:17 +0800

    h5

--- baselibrary/src/main/java/com/appbaselib/base/Html5Activity.java
+++ baselibrary/src/main/java/com/appbaselib/base/Html5Activity.java
@@ -1,30 +1,69 @@
 package com.appbaselib.base;
 
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.os.Message;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.GeolocationPermissions;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 
+import com.alibaba.android.arouter.facade.annotation.Autowired;
+import com.alibaba.android.arouter.facade.annotation.Route;
 import com.pangu.appbaselibrary.R;
 
 /**
  * Created by tangming on 2018/5/18.
  */
-
+@Route(path = "/web/Html5Activity")
 public class Html5Activity extends BaseActivity {
 
-    private String mUrl;
-    private LinearLayout mLayout;
-    private WebView mWebView;
+    @Autowired
+    String url;
+    ;
 
+    private Toolbar mToolbar;
+    private WebView mWebView;
+    ProgressBar mProgressBar;
 
     @Override
     public Toolbar getToolbar() {
-        return null;
+        return mToolbar;
     }
 
     @Override
     protected void initView() {
+
+        Log.d("Url:", url);
+
+        mToolbar = findViewById(R.id.toolbar);
+        mProgressBar = findViewById(R.id.progress);
+        mWebView=findViewById(R.id.webview);
+
+        WebSettings mWebSettings = mWebView.getSettings();
+        mWebSettings.setSupportZoom(true);
+        mWebSettings.setLoadWithOverviewMode(true);
+        mWebSettings.setUseWideViewPort(true);
+        mWebSettings.setDefaultTextEncodingName("utf-8");
+        mWebSettings.setLoadsImagesAutomatically(true);
+
+        saveData(mWebSettings);
+        newWin(mWebSettings);
+
+        mWebView.setWebChromeClient(webChromeClient);
+        mWebView.setWebViewClient(webViewClient);
+        //调用JS方法.安卓版本大于17,加上注解 @JavascriptInterface
+        mWebSettings.setJavaScriptEnabled(true);
+
+        mWebView.loadUrl(url);
 
     }
 
@@ -37,4 +76,132 @@ public class Html5Activity extends BaseActivity {
     protected View getLoadingTargetView() {
         return null;
     }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mWebView.onPause();
+        mWebView.pauseTimers(); //小心这个！！！暂停整个 WebView 所有布局、解析、JS。
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mWebView.onResume();
+        mWebView.resumeTimers();
+    }
+
+    /**
+     * 多窗口的问题
+     */
+    private void newWin(WebSettings mWebSettings) {
+        //html中的_bank标签就是新建窗口打开，有时会打不开，需要加以下
+        //然后 复写 WebChromeClient的onCreateWindow方法
+        mWebSettings.setSupportMultipleWindows(false);
+        mWebSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+    }
+
+    /**
+     * HTML5数据存储
+     */
+    private void saveData(WebSettings mWebSettings) {
+        //有时候网页需要自己保存一些关键数据,Android WebView 需要自己设置
+        mWebSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        mWebSettings.setDomStorageEnabled(true);
+        mWebSettings.setDatabaseEnabled(true);
+        mWebSettings.setAppCacheEnabled(true);
+        String appCachePath = getApplicationContext().getCacheDir().getAbsolutePath();
+        mWebSettings.setAppCachePath(appCachePath);
+    }
+
+    WebViewClient webViewClient = new WebViewClient() {
+
+        /**
+         * 多页面在同一个WebView中打开，就是不新建activity或者调用系统浏览器打开
+         */
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+    };
+
+    WebChromeClient webChromeClient = new WebChromeClient() {
+
+        //=========HTML5定位==========================================================
+        //需要先加入权限
+        //<uses-permission android:name="android.permission.INTERNET"/>
+        //<uses-permission android:name="android.permission.ACCESS_FINE_LOCATION"/>
+        //<uses-permission android:name="android.permission.ACCESS_COARSE_LOCATION"/>
+
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            super.onProgressChanged(view, newProgress);
+
+            if (newProgress == 100) {
+                // 网页加载完成
+                mProgressBar.setVisibility(View.GONE);//加载完网页进度条消失
+            } else {
+                // 加载中
+                mProgressBar.setVisibility(View.VISIBLE);//开始加载网页时显示进度条
+                mProgressBar.setProgress(newProgress);//设置进度值
+            }
+
+        }
+
+        @Override
+        public void onReceivedIcon(WebView view, Bitmap icon) {
+            super.onReceivedIcon(view, icon);
+        }
+
+        @Override
+        public void onGeolocationPermissionsHidePrompt() {
+            super.onGeolocationPermissionsHidePrompt();
+        }
+
+        @Override
+        public void onGeolocationPermissionsShowPrompt(final String origin, final GeolocationPermissions.Callback callback) {
+            callback.invoke(origin, true, false);//注意个函数，第二个参数就是是否同意定位权限，第三个是是否希望内核记住
+            super.onGeolocationPermissionsShowPrompt(origin, callback);
+        }
+        //=========HTML5定位==========================================================
+
+        //=========多窗口的问题==========================================================
+        @Override
+        public boolean onCreateWindow(WebView view, boolean isDialog, boolean isUserGesture, Message resultMsg) {
+            WebView.WebViewTransport transport = (WebView.WebViewTransport) resultMsg.obj;
+            transport.setWebView(view);
+            resultMsg.sendToTarget();
+            return true;
+        }
+        //=========多窗口的问题==========================================================
+    };
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && mWebView.canGoBack()) {
+            mWebView.goBack();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (mWebView != null) {
+            mWebView.clearHistory();
+            ((ViewGroup) mWebView.getParent()).removeView(mWebView);
+            mWebView.loadUrl("about:blank");
+            mWebView.stopLoading();
+            mWebView.setWebChromeClient(null);
+            mWebView.setWebViewClient(null);
+            mWebView.destroy();
+            mWebView = null;
+        }
+    }
+
 }
