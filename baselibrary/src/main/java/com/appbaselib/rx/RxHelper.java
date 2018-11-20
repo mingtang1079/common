256afb83b28a1e3506c79be854e1c167f4b23c1f
tangming <1079240451@qq.com>
2018-11-20 12:39:47 +0800

    加入新的网络回调

--- baselibrary/src/main/java/com/appbaselib/rx/RxHelper.java
+++ baselibrary/src/main/java/com/appbaselib/rx/RxHelper.java
@@ -21,6 +21,8 @@ import com.uber.autodispose.SingleScoper;
 import com.uber.autodispose.SingleSubscribeProxy;
 import com.uber.autodispose.android.lifecycle.AndroidLifecycleScopeProvider;
 
+import org.reactivestreams.Subscriber;
+
 import io.reactivex.Completable;
 import io.reactivex.Flowable;
 import io.reactivex.Maybe;
@@ -33,6 +35,7 @@ import io.reactivex.Single;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
 import io.reactivex.parallel.ParallelFlowable;
 import io.reactivex.schedulers.Schedulers;
 
@@ -41,7 +44,7 @@ import static com.uber.autodispose.ScopeUtil.deferredResolvedLifecycle;
 public class RxHelper {
 
     /**
-     * 对结果进行预处理 (不带生命周期) 返回全部实体
+     * 不对结果进行预处理 (不带生命周期) 返回全部实体
      *
      * @param <T>
      * @return
@@ -67,7 +70,37 @@ public class RxHelper {
     }
 
     /**
-     * 对结果进行预处理 (带生命周期)
+     * 对结果进行预处理 (不带生命周期) 返回全部实体
+     *     * 需要手动调用as操作符  .as(AutoDispose.<T>autoDisposable(AndroidLifecycleScopeProvider.from(context))),回调请使用 MySubscirbe 类
+     * @param <T>
+     * @return
+     */
+    public static <T> ObservableTransformer<BaseModel<T>, T> handleResult2() {
+
+        return new ObservableTransformer<BaseModel<T>, T>() {
+            @Override
+            public ObservableSource<T> apply(Observable<BaseModel<T>> upstream) {
+
+             return    upstream.flatMap(new Function<BaseModel<T>, ObservableSource<T>>() {
+                    @Override
+                    public ObservableSource<T> apply(BaseModel<T> mTBaseModel) throws Exception {
+                        if (mTBaseModel.getCode() == 200)
+                            return createData(mTBaseModel.getData());
+                        else {
+                            return Observable.error(new ServerException(mTBaseModel.getMsg()));
+                        }
+
+                    }
+                }).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
+
+            }
+        };
+
+    }
+
+
+    /**
+     * 不对结果进行预处理 (带生命周期)
      *
      * @param <T>
      * @return
@@ -114,6 +147,44 @@ public class RxHelper {
     }
 
 
+    private static <T> AutoDisposeConverter<T> handleResult2(Context mContext){
+        final Maybe scope = deferredResolvedLifecycle(NullUtils.checkNotNull(AndroidLifecycleScopeProvider.from((LifecycleOwner) mContext), "provider == null"));
+        NullUtils.checkNotNull(scope, "scope == null");
+
+        return new AutoDisposeConverter<T>() {
+            @Override
+            public ParallelFlowableSubscribeProxy<T> apply(ParallelFlowable<T> upstream) {
+                return null;
+            }
+
+            @Override
+            public SingleSubscribeProxy<T> apply(Single<T> upstream) {
+                return null;
+            }
+
+            @Override
+            public ObservableSubscribeProxy<T> apply(Observable<T> upstream) {
+                return upstream.to(new ObservableScoper<T>(scope));
+            }
+
+            @Override
+            public MaybeSubscribeProxy<T> apply(Maybe<T> upstream) {
+                return null;
+            }
+
+            @Override
+            public FlowableSubscribeProxy<T> apply(Flowable<T> upstream) {
+                return null;
+            }
+
+            @Override
+            public CompletableSubscribeProxy apply(Completable upstream) {
+                return null;
+            }
+        };
+
+    }
+
     /**
      * 线程切换的封装
      *
@@ -129,6 +200,29 @@ public class RxHelper {
                         .observeOn(AndroidSchedulers.mainThread());
             }
         };
+    }
+
+    /**
+     * 创建成功的数据
+     *
+     * @param data
+     * @param <T>
+     * @return
+     */
+    private static <T> Observable<T> createData(final T data) {
+
+        return Observable.create(new ObservableOnSubscribe<T>() {
+            @Override
+            public void subscribe(ObservableEmitter<T> e) throws Exception {
+                try {
+                    e.onNext(data);
+                    e.onComplete();
+                } catch (Exception ex) {
+                    e.onError(ex);
+                }
+            }
+        });
+
     }
 
 
