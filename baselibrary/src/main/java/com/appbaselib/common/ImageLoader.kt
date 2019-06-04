1477b6eb6fda0b764ec009795dccff63d935d81a
pu <38392068+jwptest@users.noreply.github.com>
2019-06-04 16:53:59 +0800

    glide新增加载圆角

--- baselibrary/src/main/java/com/appbaselib/common/ImageLoader.kt
+++ baselibrary/src/main/java/com/appbaselib/common/ImageLoader.kt
@@ -6,30 +6,40 @@ import android.net.Uri
 import android.widget.ImageView
 
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
 import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.RequestOptions
+import com.pangu.appbaselibrary.R
 
 /**
  * Created by CLOUD on 2016/10/14.
  */
 
 object ImageLoader {
-
     fun load(context: Context, path: String, imageView: ImageView) {
         Glide.with(context).load(path)
                 //  .centerCrop()  //
-                //                .placeholder(R.drawable.image_loading)
                 //                .error(R.drawable.image_failure)
                 .transition(withCrossFade())
                 .into(imageView)
 
     }
 
+    fun load(context: Context, path: String, imageView: ImageView,corner:Float) {
+        Glide.with(context).load(path)
+                //  .centerCrop()  //
+                .apply(RequestOptions().transforms(CenterCrop(), RoundCorner(context, 12f)))
+             //   .transition(withCrossFade())
+                .into(imageView)
+
+    }
+
+
     fun load(context: Context, path: Uri, imageView: ImageView) {
         Glide.with(context).load(path)
                 //  .centerCrop()  //
                 .transition(withCrossFade())
-                //                .placeholder(R.drawable.image_loading)
                 //                .error(R.drawable.image_failure)
                 .into(imageView)
 
