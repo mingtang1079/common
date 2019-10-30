a70a8f4899d5a17274d690adf060cd2cffaaca81
pu <38392068+jwptest@users.noreply.github.com>
2019-10-30 10:19:18 +0800

    测试

---
+++ baselibrary/src/main/java/com/appbaselib/A.java
@@ -0,0 +1,14 @@
+package com.appbaselib;
+
+import java.io.PushbackInputStream;
+
+/**
+ * ===============================
+ * 描    述：
+ * 作    者：pjw
+ * 创建日期：2019/10/30 10:18
+ * ===============================
+ */
+public class A {
+    public  String a;
+}
